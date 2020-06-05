e213d4edff7a5382858b6fc135de88a37eaa737f
kennethormandy <hello@kennethormandy.com>
2020-06-05 16:12:46 -0700

    Fixes exporting of function for Babel 7

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ Object.defineProperty(module.exports, 'buildPreset', {
 function preset(context, opts) {
   const env = require('@babel/preset-env');
 
-  return {
+  return () => ({
     presets: [env],
     plugins: [require('./node-require.js')].filter(Boolean),
-  };
+  });
 }
